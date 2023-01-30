337a08660b5d302d125c89c051559d08faa3c673
aelieber1 <80122879+aelieber1@users.noreply.github.com>
2023-01-29 22:53:01 -0600

    Add files via upload

---
+++ PHSX815_Week2-master/python/DiceRoll.py
@@ -0,0 +1,138 @@
+# This code simulates a dice roll that has say 4 sides. A new method has been added to the Random class which generates random numbers according to a categorical distribution. Code was adapted for these purposes from a tutorial on categorical distributions found here: https://www.youtube.com/watch?v=MYHxW-MIld0
+# The sides of the dice reads as follows: {0,1,2,3}
+
+#! /usr/bin/env python
+
+# imports of external packages to use in our code
+import sys
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import our Random class from python/Random.py file
+sys.path.append(".")
+from Random import Random
+
+# main function for our Dice roll Python code
+if __name__ == "__main__":
+    # if the user includes the flag -h or --help print the options
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print ("Usage: %s [-seed number]" % sys.argv[0])
+        print
+        sys.exit(1)
+    
+    # default seed
+    seed = 5555
+    
+    # default number of rolls
+    n_samples = 10
+    
+    # default single dice roll probability for hypothesis 0
+    p0 = 0.25
+
+    # default single dice roll probability for hypothesis 1
+    p1 = 0.25
+    
+    # default single dice roll probability for hypothesis 2
+    p2 = 0.25
+    
+    # default single dice roll probability for hypothesis 4
+    p3 = 0.25
+
+    haveH0 = False
+    haveH1 = False
+    haveH2 = False
+    haveH3 = False
+    
+    if '-Nroll' in sys.argv:
+        p = sys.argv.index('-Nroll')
+        Nr = int(sys.argv[p+1])
+        if Nr > 0:
+            n_samples = Nr
+    if '-prob0' in sys.argv:
+        p = sys.argv.index('-prob0')
+        ptemp = float(sys.argv[p+1])
+        if ptemp >= 0 and ptemp <= 1:
+            p0 = ptemp
+    if '-prob1' in sys.argv:
+        p = sys.argv.index('-prob1')
+        ptemp = float(sys.argv[p+1])
+        if ptemp >= 0 and ptemp <= 1:
+            p1 = ptemp
+    if '-prob2' in sys.argv:
+        p = sys.argv.index('-prob2')
+        ptemp = float(sys.argv[p+1])
+        if ptemp >= 0 and ptemp <= 1:
+            p2 = ptemp
+    if '-prob3' in sys.argv:
+        p = sys.argv.index('-prob3')
+        ptemp = float(sys.argv[p+1])
+        if ptemp >= 0 and ptemp <= 1:
+            p3 = ptemp
+    if '-input0' in sys.argv:
+        p = sys.argv.index('-input0')
+        InputFile0 = sys.argv[p+1]
+        haveH0 = True
+    if '-input1' in sys.argv:
+        p = sys.argv.index('-input1')
+        InputFile1 = sys.argv[p+1]
+        haveH1 = True
+    if '-input2' in sys.argv:
+        p = sys.argv.index('-input1')
+        InputFile2 = sys.argv[p+1]
+        haveH2 = True
+    if '-input3' in sys.argv:
+        p = sys.argv.index('-input1')
+        InputFile3 = sys.argv[p+1]
+        haveH3 = True
+    #if '-h' in sys.argv or '--help' in sys.argv or not haveH0:
+        #print ("Usage: %s [options]" % sys.argv[0])
+        #print ("  options:")
+        #print ("   --help(-h)          print options")
+        #print ("   -input0 [filename]  name of file for H0 data")
+        #print ("   -input1 [filename]  name of file for H1 data")
+        #print ("   -input2 [filename]  name of file for H2 data")
+        #print ("   -input3 [filename]  name of file for H3 data")
+        #print ("   -prob0 [number]     probability of 1 for single toss for H0")
+        #print ("   -prob1 [number]     probability of 1 for single toss for H1")
+        #print ("   -prob2 [number]     probability of 1 for single toss for H2")
+        #print ("   -prob3 [number]     probability of 1 for single toss for H3")
+        #print ("   -Nroll [number]     number of rolls")
+        #print
+        #sys.exit(1)
+        
+    # gather probabilities into an array
+    thetas = [p0, p1, p2, p3] 
+    print(thetas)
+    
+    # class instance of Random class 
+    random = Random(seed)
+    
+    # Calling for the sampling to be done using the categorical distribution
+    arr = random.dist_categorical(thetas, n_samples)
+    print(arr)
+    
+    # Adapting code from Week 1 work below to take this data that has now been randomly sampled according to a categorical distribution
+    
+    # create histogram of our data
+    n, bins, patches = plt.hist(arr, bins=50, density=True, color='lightblue', ec='C0', orientation='vertical', alpha=0.75)
+
+    # plot formating options
+    title = 'Categorical Random Sampling for: ' + str(n_samples) + " 4-sided Dice Rolls"
+    plt.xlabel('x', color='C0', fontsize=12)
+    plt.ylabel('Probability', color='C0', fontsize=12)
+    plt.title(title, color='C0', fontsize=15)
+    plt.grid(True)
+
+    # show figure (program only ends once closed
+    plt.show()
+    
+    plt.hist(arr, bins=50, density=True, color='lightblue', ec='C0', orientation='vertical', alpha=0.75)
+
+    # Formatting the plot
+    plt.xlabel('x', color='C0', fontsize=15)
+    plt.ylabel('Probability', color='C0', fontsize=15)
+    plt.title('Uniform Random Number', color='C0', fontsize=10)
+    plt.grid(True)
+    
+    plt.show()
---
+++ PHSX815_Week2-master/python/Random.py
@@ -0,0 +1,88 @@
+#! /usr/bin/env python
+
+import math
+import numpy as np
+
+#################
+# Random class
+#################
+# class that can generate random numbers
+class Random:
+    """A random number generator class"""
+
+    # initialization method for Random class
+    def __init__(self, seed = 5555):
+        self.seed = seed
+        self.m_v = np.uint64(4101842887655102017)
+        self.m_w = np.uint64(1)
+        self.m_u = np.uint64(1)
+        
+        self.m_u = np.uint64(self.seed) ^ self.m_v
+        self.int64()
+        self.m_v = self.m_u
+        self.int64()
+        self.m_w = self.m_v
+        self.int64()
+
+    # function returns a random 64 bit integer
+    def int64(self):
+        with np.errstate(over='ignore'):
+            self.m_u = np.uint64(self.m_u * np.uint64(2862933555777941757) + np.uint64(7046029254386353087))
+        self.m_v ^= self.m_v >> np.uint64(17)
+        self.m_v ^= self.m_v << np.uint64(31)
+        self.m_v ^= self.m_v >> np.uint64(8)
+        self.m_w = np.uint64(np.uint64(4294957665)*(self.m_w & np.uint64(0xffffffff))) + np.uint64((self.m_w >> np.uint64(32)))
+        x = np.uint64(self.m_u ^ (self.m_u << np.uint64(21)))
+        x ^= x >> np.uint64(35)
+        x ^= x << np.uint64(4)
+        with np.errstate(over='ignore'):
+            return (x + self.m_v)^self.m_w
+
+    # function returns a random floating point number between (0, 1) (uniform)
+    def rand(self):
+        return 5.42101086242752217E-20 * self.int64()
+
+    # function returns a random integer (0 or 1) according to a Bernoulli distr.
+    def Bernoulli(self, p=0.5):
+        if p < 0. or p > 1.:
+            return 1
+        
+        R = self.rand()
+
+        if R < p:
+            return 1
+        else:
+            return 0
+
+    # function returns a random double (0 to infty) according to an exponential distribution
+    def Exponential(self, beta=1.):
+      # make sure beta is consistent with an exponential
+      if beta <= 0.:
+        beta = 1.
+
+      R = self.rand()
+
+      while R <= 0.:
+        R = self.rand()
+
+      X = -math.log(R)/beta
+
+      return X
+    
+    # Additional distribution for Homework #3
+    # First I tried the Categorical distribution as you suggested:
+    def dist_categorical(self, thetas, n_samples):
+        n_classes = len(thetas)
+        
+        # sets the categorical boundaries
+        thetas_cumsum = np.cumsum(thetas)
+        
+        # samples a random dataset
+        dataset_x = np.random.rand(n_samples)
+        
+        # uses np.select to categorize the data to transform from data sampled from a uniform distribution to data sampled from a categorical
+        lower_than_limits = [dataset_x <= limit for limit in thetas_cumsum]
+        class_matrix = [i * np.ones(n_samples) for i in range(n_classes)]
+        dataset_w = np.select(condlist=lower_than_limits, choicelist=class_matrix)
+        
+        return dataset_w
